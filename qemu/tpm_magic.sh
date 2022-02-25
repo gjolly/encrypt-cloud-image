9daef0c3fdc911dd5c33590f21e0da86a3326c12
Gauthier Jolly <gauthier.jolly@canonical.com>
2022-02-25 17:40:19 +0100

    do not use a password with tpm2_createprimary

--- qemu/tpm_magic.sh
+++ qemu/tpm_magic.sh
@@ -10,13 +10,12 @@ fi
 ############### TPM setup ###############
 tpm2_clear
 
-# In theory, should use a secure password
 tpm2_changeauth -c owner
 
 tpm2_createek -c ek.ctx
-tpm2_createprimary -C o -c ek.ctx -P password
+tpm2_createprimary -C o -c ek.ctx
 
-tpm2_evictcontrol -P password -c ek.ctx > handle.yaml
+tpm2_evictcontrol -c ek.ctx > handle.yaml
 handle="$(grep 'persistent-handle' handle.yaml | cut -d':' -f2 | xargs)"
 
 tpm2_readpublic -c "$handle" -o srk.pub > srk.yaml
